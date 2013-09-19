be704b4d2d8cdf452ad8a03ebfd7284eddd7e534
Sajjad Anwar <sajjadkm@gmail.com>
2013-09-19 18:29:34 +0530

    Work in progress

--- disetulna/__init__.py
+++ disetulna/__init__.py
@@ -4,10 +4,24 @@
 
 from __future__ import absolute_import
 from flask import Flask
+from flask.ext.assets import Environment, Bundle
 
 # First, make an app
 
 app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('settings.py', silent=True)
+assets = Environment(app)
+
+css = Bundle('js/libs/bootstrap/css/bootstrap.min.css',
+	'css/app.css')
+
+js = Bundle('js/libs/jquery/jquery1.10.js',
+			'js/libs/bootstrap/js/bootstrap.min.js',
+			'js/app.js',
+            filters='jsmin', output='js/packed.js')
+
+assets.register('js_all', js)
+assets.register('css_all', css)
 
 # Second, import the models and views
 
--- disetulna/views/index.py
+++ disetulna/views/index.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from .. import app
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', title=app.config['SITE_TITLE'])
+	if request.method == 'GET':
+		
+		return render_template('index.html')
+	else:
+		print request.form['dise']
+		return redirect(url_for('clicked'), code=303)
+
+
+@app.route('/clicked')
+def clicked():
+	return render_template('clicked.html')
