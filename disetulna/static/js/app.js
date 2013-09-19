77663d525e9f00ba729d61634f3af5d3925b58dc
Sajjad Anwar <sajjadkm@gmail.com>
2013-09-19 20:36:48 +0530

    Index page and post request

--- disetulna/static/js/app.js
+++ disetulna/static/js/app.js
@@ -1,7 +1,23 @@
-// function clicked () {
-// 	console.log('hello');
-// 	$.ajax({
-// 		type: "POST",
-// 		url: "/",
-// 	});
-// }
+function change_focus(id,type,flag)
+{
+	var data='';
+	var element=document.getElementById(type);
+	if(type=='dise_blk')
+		data=dise_block;
+	else if(type=='dise_clust')
+		data=dise_clust;
+	else if(type=='klp_blk')
+		data=klp_block;
+	else if(type=='klp_clust')
+		data=klp_clust;
+	element.length=1;
+	for(var i=0;i<data.length-1;i++){
+		if(data[i][0]==id){
+			if(flag==1)
+				element.options[element.length]=new Option(data[i][2],data[i][1]);
+			else{
+				element.options[element.length]=new Option(data[i][1],data[i][1]);
+			}
+		}
+	}
+}
--- disetulna/views/index.py
+++ disetulna/views/index.py
@@ -1,19 +1,51 @@
 # -*- coding: utf-8 -*-
 
+import csv
 from flask import render_template, request, redirect, url_for
 from .. import app
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	if request.method == 'GET':
-		
-		return render_template('index.html')
-	else:
-		print request.form['dise']
-		return redirect(url_for('clicked'), code=303)
+    if request.method == 'GET':
+        fp1=csv.reader(open('disetulna/static/data/dise-sample.csv','r'),delimiter='|')
+        fp2=csv.reader(open('disetulna/static/data/klp-sample.csv','r'),delimiter='|')       
+        
+        dise_district=[]
+        dise_block=[]
+        dise_cluster=[]
+        
+        schools=[row for row in fp1]
+        for row in schools:
+            if row[0] not in dise_district:
+                dise_district.append(row[0])
+        for row in schools:
+            if [row[0],row[1]] not in dise_block:
+                dise_block.append([row[0],row[1]])
+        for row in schools:
+            if [row[1],row[2]] not in dise_cluster:
+                dise_cluster.append([row[1],row[2]])
+
+        klp_district=[]
+        klp_block=[]
+        klp_cluster=[]
+        
+        schools=[row for row in fp2]
+        for row in schools:
+            if row[0] not in klp_district:
+                klp_district.append(row[0])
+        for row in schools:
+            if [row[0],row[1]] not in klp_block:
+                klp_block.append([row[0],row[1]])
+        for row in schools:
+            if [row[1],row[2]] not in klp_cluster:
+                klp_cluster.append([row[1],row[2]])     
+        return render_template('index.html', dise_district=dise_district,dise_block=dise_block,dise_cluster=dise_cluster,klp_district=klp_district,klp_block=klp_block,klp_cluster=klp_cluster)
+    else:
+        print request.form['dise']
+        return redirect(url_for('clicked'), code=303)
 
 
 @app.route('/clicked')
 def clicked():
-	return render_template('clicked.html')
+    return render_template('clicked.html')
