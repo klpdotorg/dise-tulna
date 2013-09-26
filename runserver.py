87fd27e1768e4cd275f86506d31c028ce59d3cd3
BRIJESH <brijesh@ubuntu.ubuntu-domain>
2013-09-26 14:27:22 +0530

    activated db.create_all()

--- runserver.py
+++ runserver.py
@@ -6,6 +6,6 @@ from os import environ
 # environ['PEOPLEFLOW_ENV'] = 'dev'
 
 if __name__=='__main__':
-    # db.create_all()
+    db.create_all()
     app.config['ASSETS_DEBUG'] = True
     app.run('0.0.0.0', 8000, debug=True)
